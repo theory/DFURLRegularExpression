1223556e0c271993491a435a42571d163dd1a0a7
David E. Wheeler <david@justatheory.com>
2011-10-19 23:34:54 -0700

    Convert to ARC.

--- Classes/DFURLRegularExpression.m
+++ Classes/DFURLRegularExpression.m
@@ -16,17 +16,17 @@ static NSRegularExpression *WebURLRegex = nil;
 
 + (void)initialize {
     if (self == [DFURLRegularExpression class]) {
-        URLRegex = [[NSRegularExpression
+        URLRegex = [NSRegularExpression
             regularExpressionWithPattern:@"\\b((?:[a-z][\\w-]+:(?:/{1,3}|[a-z0-9%])|www\\d{0,3}[.]|[a-z0-9.\\-]+[.][a-z]{2,4}/)(?:[^\\s()<>]+|\\(([^\\s()<>]+|(\\([^\\s()<>]+\\)))*\\))+(?:\\(([^\\s()<>]+|(\\([^\\s()<>]+\\)))*\\)|[^\\s`!()\\[\\]{};:'\".,<>?«»“”‘’]))"
                     options:NSRegularExpressionCaseInsensitive
                     error:nil
-        ]retain];
+        ];
 
-        WebURLRegex = [[NSRegularExpression
+        WebURLRegex = [NSRegularExpression
             regularExpressionWithPattern:@"\\b((?:https?://|www\\d{0,3}[.]|[a-z0-9.\\-]+[.][a-z]{2,4}/)(?:[^\\s()<>]+|\\(([^\\s()<>]+|(\\([^\\s()<>]+\\)))*\\))+(?:\\(([^\\s()<>]+|(\\([^\\s()<>]+\\)))*\\)|[^\\s`!()\\[\\]{};:'\".,<>?«»“”‘’]))"
                        options:NSRegularExpressionCaseInsensitive
                        error:nil
-        ]retain];
+        ];
     }
 }
 
